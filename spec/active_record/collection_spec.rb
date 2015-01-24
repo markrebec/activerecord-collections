4903a05a3285bd85736e2204096eae31f60ff7e9
Mark Rebec <mark@markrebec.com>
2015-01-24 15:44:05 -0800

    adds a simple spec to ensure querying returns the right records

--- spec/active_record/collection_spec.rb
+++ spec/active_record/collection_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 RSpec.describe ActiveRecord::Collection do
+  context 'querying' do
+    before(:each) { create(:retailer) }
+
+    it 'should return the same records as a standard relation' do
+      retailer = Retailer.all.to_a.sample
+      collection = StockedProducts.where(retailer_id: retailer.id)
+      relation = StockedProduct.where(retailer_id: retailer.id)
+      expect(collection.count).to eql(relation.count)
+      expect(collection.pluck(:id).sort).to eql(relation.pluck(:id).sort)
+    end
+  end
 end
