7298635c09586dc63ccbc413f636213ab81eb9b8
Mark Rebec <mark@markrebec.com>
2015-01-24 15:04:06 -0800

    adds collection model and associations for specs

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -1,15 +1,26 @@
 class Retailer < ActiveRecord::Base
+  has_many :stocked_products
 end
 
 class StoreLocation < ActiveRecord::Base
   belongs_to :retailer
+  has_many :stocked_products
 end
 
 class Product < ActiveRecord::Base
+  has_many :stocked_products
 end
 
 class StockedProduct < ActiveRecord::Base
   belongs_to :product
   belongs_to :store_location
   belongs_to :retailer
+end
+
+class StockedProducts < ActiveRecord::Collection
+  protected
+
+  def initialize(*criteria)
+    super(StockedProduct, *criteria)
+  end
 end
